0032b573ff15adebcdda3d5a39046c6d1184ba0d
ToSun <liuxybb@163.com>
2015-09-23 17:28:26 +0800

    fix bug

--- app/controllers/wx.js
+++ app/controllers/wx.js
@@ -10,7 +10,7 @@ module.exports = function(app){
   app.use('/', router);
 };
 
-router.use('/wx', wechat(config.wechat.token, function (req, res, next) {
+router.use('/wx', wechat(config.wechat, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
   if (message.FromUserName === 'diaosi') {
